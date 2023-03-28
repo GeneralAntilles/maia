febb1cffa86401a3c1260c391ee6a30b9899d747
Ryan Abel <ryan@talkabel.com>
2023-03-27 20:47:56 -0500

    Update admin interface for new models

--- maia_v2/admin.py
+++ maia_v2/admin.py
@@ -1,6 +1,32 @@
 from django.contrib import admin
 
-from .models import QuestionCategory, Question
+from .models import (
+    Questionnaire, QuestionCategory, Question,
+    User,
+    QuestionnaireResponse, QuestionResponse,
+)
 
-admin.site.register(QuestionCategory)
-admin.site.register(Question)
+
+class QuestionCategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'internal_name', 'questionnaire', 'questions')
+    search_fields = ('name', 'internal_name')
+    list_filter = ('questionnaire',)
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'text', 'category', 'questionnaire', 'reverse_score')
+    search_fields = ('text',)
+    list_filter = ('questionnaire', 'category')
+
+
+class QuestionnaireAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'internal_name', 'questions')
+    search_fields = ('name', 'internal_name')
+
+
+admin.site.register(QuestionCategory, QuestionCategoryAdmin)
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Questionnaire, QuestionnaireAdmin)
+admin.site.register(User)
+admin.site.register(QuestionnaireResponse)
+admin.site.register(QuestionResponse)
