3692cf82270ac772c6eeda4571c4509fce85c039
Ryan Abel <ryan@talkabel.com>
2023-04-04 23:39:41 -0500

    Use generic custom form fetching in view
    This way we can get any questionnaire from based on the URL.

--- maia_v2/views.py
+++ maia_v2/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.views.generic import View
 
-from .forms import MAIAForm
+from .forms import get_questionnaire_form
 from .models import Question, QuestionCategory, Questionnaire
 
 
@@ -14,10 +14,13 @@ def index(request):
 
 class QuestionnaireFormView(View):
     def get(self, request, questionnaire):
+        form = get_questionnaire_form(questionnaire)
         self.questionnaire = Questionnaire.objects.get(internal_name=questionnaire)
         response = {
+            'questionnaire': self.questionnaire,
             'questions': Question.objects.filter(questionnaire=self.questionnaire),
             'categories': QuestionCategory.objects.filter(questionnaire=self.questionnaire),
+            'form': form,
         }
 
         return render(request, f'maia_v2/{questionnaire}.html', response)
