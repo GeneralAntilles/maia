063fdffa938ab39dd393ac8b7fcdec00b5d51ad3
Ryan Abel <ryan@talkabel.com>
2023-04-10 22:08:12 -0500

    Incorrect field name

--- maia_v2/admin.py
+++ maia_v2/admin.py
@@ -25,14 +25,14 @@ class QuestionAdmin(admin.ModelAdmin):
 
 
 class QuestionnaireAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'internal_name', 'description',
-                    'question_list')
+    list_display = ('id', 'name', 'internal_name', 'description', 'scale',
+                    'scale_max', 'scale_min', 'question_list')
     search_fields = ('name', 'internal_name')
     readonly_fields = ('question_list',)
     fieldsets = (
         (None, {
             'fields': ('name', 'internal_name', 'description', 'instructions',
-                       'scale', 'score_max', 'score_min', 'question_list'),
+                       'scale', 'scale_max', 'scale_min', 'question_list'),
         }),
     )
 
