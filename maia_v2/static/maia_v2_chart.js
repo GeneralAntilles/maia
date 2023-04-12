e00363a337163ddbf30df421b04929b65eaa7ce1
Ryan Abel <ryan@talkabel.com>
2023-04-11 23:08:04 -0500

    Add percentile and histogram chart to results page

--- maia_v2/static/maia_v2_chart.js
+++ maia_v2/static/maia_v2_chart.js
@@ -1,4 +1,26 @@
-var chart = bb.generate({
+// Histogram chart comparing against the overall total score distribution
+// of all users
+var histogram_chart = bb.generate({
+    data: {
+        url: "./stats/?format=json",
+        mimeType: "json",
+        keys: { x: "data1", value: ["Respondents"] },
+        type: "bar",
+    },
+    axis: {
+        y: { padding: 0 }
+    },
+    bar: { width: { ratio: 1 } },
+    legend: { hide: true },
+    color: function(color, d) {
+        console.log(d);
+        if (d.value == total_score) { return "#ff0000"; }
+        return color;
+    },
+    bindto: "#result-hist-chart",
+});
+
+var bar_chart = bb.generate({
     data: {
         url: "./scores/?format=json",
         mimeType: "json",
@@ -6,9 +28,24 @@ var chart = bb.generate({
         type: "bar",
     },
     axis: {
-        x: { type: "category" },
+        x: {
+            type: "category", tick: {
+                format: function (index, categoryName) {
+                    // Add a newline after a hyphen if there's no space
+                    return categoryName.replace("-", "-\n");
+                }
+            }
+        },
         y: { min: 0, max: max_score, padding: 0 }
     },
-    legend: { show: false },
-    bindto: "#result-bar-chart"
+    legend: { position: "bottom" },
+    bindto: "#result-bar-chart",
+    oninit: function() {
+        this.load({
+            url: "./comparison/?format=json",
+            mimeType: "json",
+            keys: { x: "name", value: ["Museum visitors", "Hospital pre-treatment", "Hospital post-treatment"] },
+            type: "bar",
+        });
+    }
 });
