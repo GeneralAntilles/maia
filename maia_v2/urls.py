c51f3ab3cf7fad2e65ec68e35c633eeeb62df9cf
Ryan Abel <ryan@talkabel.com>
2023-11-27 21:31:36 -0600

    Add media folder to URLs

--- maia_v2/urls.py
+++ maia_v2/urls.py
@@ -1,4 +1,6 @@
+from django.conf import settings
 from django.urls import path
+from django.conf.urls.static import static
 
 from . import views
 
@@ -35,4 +37,4 @@ urlpatterns = [
         views.APIQuestionnaireStatsView.as_view(),
         name='questionnaire-stats',
     ),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
